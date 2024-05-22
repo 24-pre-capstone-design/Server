30e8a3f2afde62053ade53d7452b405197e631e6
석해현 <gogus4600@kyonggi.ac.kr>
2024-05-22 14:41:19 +0900

    feat: 직원 조회 기능 추가 #64

--- src/main/java/com/pre_capstone_design_24/server/controller/EmployeeController.java
+++ src/main/java/com/pre_capstone_design_24/server/controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.pre_capstone_design_24.server.controller;
 
+import com.pre_capstone_design_24.server.domain.EmployeeStatus;
 import com.pre_capstone_design_24.server.global.response.ApiResponse;
 import com.pre_capstone_design_24.server.global.response.Status;
 import com.pre_capstone_design_24.server.requestDto.EmployeeRequestDto;
+import com.pre_capstone_design_24.server.responseDto.EmployeeResponseDto;
+import com.pre_capstone_design_24.server.responseDto.OwnerResponseDto;
 import com.pre_capstone_design_24.server.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,13 +38,46 @@ public class EmployeeController {
         return ApiResponse.onSuccess(Status.CREATED.getCode(), Status.CREATED.getMessage(), null);
     }
 
+    @Operation(summary = "ID로 직원 조회")
+    @Secured({"ROLE_USER"})
+    @GetMapping("{employeeId}")
+    public ApiResponse<?> getEmployeeById(
+        @PathVariable("employeeId") Long employeeId
+    ) {
+        EmployeeResponseDto employeeResponseDto = employeeService.getEmployeeById(employeeId);
+        return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), employeeResponseDto);
+    }
+
+    @Operation(summary = "이름으로 직원 조회")
+    @Secured({"ROLE_USER"})
+    @GetMapping("/by-name")
+    public ApiResponse<?> getEmployeeByName(@RequestParam("name") String name) {
+        EmployeeResponseDto employeeResponseDto = employeeService.getEmployeeByName(name);
+        return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), employeeResponseDto);
+    }
+
+    @Operation(summary = "날짜로 직원 조회")
+    @Secured({"ROLE_USER"})
+    @GetMapping("/by-work-date")
+    public ApiResponse<?> getEmployeesByWorkDate(@RequestParam("workDate") String workDate) {
+        List<EmployeeResponseDto> employees = employeeService.getEmployeesByWorkDate(workDate);
+        return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), employees);
+    }
+
+    @Operation(summary = "상태로 직원 조회")
+    @Secured({"ROLE_USER"})
+    @GetMapping("/by-status")
+    public ApiResponse<?> getEmployeesByStatus(@RequestParam("status") EmployeeStatus status) {
+        List<EmployeeResponseDto> employees = employeeService.getEmployeesByStatus(status);
+        return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), employees);
+    }
+
     @Operation(summary = "모든 직원 조회")
     @Secured({"ROLE_USER"})
     @GetMapping("/all")
     public ApiResponse<?> getAllEmployees() {
         return ApiResponse.onSuccess(Status.OK.getCode(), Status.OK.getMessage(), employeeService.getAllEmployees());
     }
-
 
     @Operation(summary = "직원 수정")
     @Secured({"ROLE_USER"})
--- src/main/java/com/pre_capstone_design_24/server/repository/EmployeeRepository.java
+++ src/main/java/com/pre_capstone_design_24/server/repository/EmployeeRepository.java
@@ -1,12 +1,23 @@
 package com.pre_capstone_design_24.server.repository;
 
 import com.pre_capstone_design_24.server.domain.Employee;
+import com.pre_capstone_design_24.server.domain.EmployeeStatus;
 import com.pre_capstone_design_24.server.domain.Owner;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findAllByOwner(Owner owner);
+
+    Optional<Employee> findByNameAndOwner(String name, Owner owner);
+
+    @Query("SELECT e FROM Employee e WHERE e.workDate LIKE %:workDate% AND e.owner = :owner")
+    List<Employee> findAllByWorkDateAndOwner(@Param("workDate") String workDate, @Param("owner") Owner owner);
+
+    List<Employee> findAllByStatusAndOwner(EmployeeStatus status, Owner owner);
 }
