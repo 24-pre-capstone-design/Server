586ba068aa6cb69412b1129ce0b84bc86f08342d
mingmingmon <mingmingmon@naver.com>
2024-06-02 14:46:12 +0900

    fix: Owner 비밀번호 정책 수정 #70

---
+++ src/main/java/com/pre_capstone_design_24/server/requestDto/OwnerUpdateDto.java
@@ -0,0 +1,25 @@
+package com.pre_capstone_design_24.server.requestDto;
+
+import com.pre_capstone_design_24.server.domain.Password;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OwnerUpdateDto {
+
+    @NotNull
+    @Schema(description = "사장님 이름", example = "전민주")
+    private String name;
+
+    @NotNull
+    @Schema(description = "생년월일", example = "20020722")
+    private String birthDate;
+
+}
--- src/main/java/com/pre_capstone_design_24/server/responseDto/OwnerResponseDto.java
+++ src/main/java/com/pre_capstone_design_24/server/responseDto/OwnerResponseDto.java
@@ -1,6 +1,8 @@
 package com.pre_capstone_design_24.server.responseDto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pre_capstone_design_24.server.domain.Owner;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,11 +22,15 @@ public class OwnerResponseDto {
 
     String birthDate;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    LocalDateTime createdAt;
+
     public static OwnerResponseDto of(Owner owner) {
         return OwnerResponseDto.builder()
                 .id(owner.getId())
                 .name(owner.getName())
                 .birthDate(owner.getBirthDate())
+                .createdAt(owner.getCreatedAt())
                 .build();
     }
 
